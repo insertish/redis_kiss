f6c262996799259b2993c6d44f797ba6955af4c7
Paul Makles <paulmakles@gmail.com>
2022-02-05 18:45:11 +0000

    feat: inital commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# redis_kiss
+
+This is a pretty simple library intended for just consuming and producing messages for Redis PubSub, it does not follow "good programming practice" in the slightest but it does work and does simplify the work required.
+
+The library manages a global pool of Redis connections and creates listener connections on the fly (due to some technical limitations).
+
+## Example
+
+Publish something to Redis:
+
+```rust
+// Handle the Result<_, _>
+redis_kiss::publish("channel", "data").await?;
+
+// Log the error instead
+redis_kiss::p("channel", "data").await;
+```
+
+Subscribe to Redis (as usual, [see Redis documentation for more info](https://docs.rs/redis/latest/redis/aio/struct.PubSub.html)):
+
+```rust
+// Create a new PubSub connection
+let mut conn = redis_kiss::open_pubsub_connection().await?;
+
+// Subscribe to something
+conn.subscribe("test").await?;
+
+// Handle incoming messages
+let mut stream = conn.on_message();
+while let Some(item) = stream.next().await {
+    // Use Redis crate API as usual
+    let channel = item.get_channel_name().to_string();
+
+    // Decode the message using previously specified options
+    // into a certain DeserializeOwned data type T, in this case String
+    let payload: String = redis_kiss::decode_payload(&item).await?;
+
+    // You can also choose a specific type on the fly
+    use redis_kiss::PayloadType;
+    let payload: String = redis_kiss::decode_payload(&item, &PayloadType::Msgpack).await?;
+
+    // Do something with channel and payload
+}
+```
+
+## Environment Variables
+
+The library is provided with options through the environment.
+
+| Name                  | Description                                                                                                  | Default             |
+| --------------------- | ------------------------------------------------------------------------------------------------------------ | ------------------- |
+| `REDIS_URI`           | URI of the Redis server                                                                                      | `redis://localhost` |
+| `REDIS_POOL_MAX_OPEN` | Maximum number of connections in Redis pool (for publishing data)                                            | `1000`              |
+| `REDIS_PAYLOAD_TYPE`  | Data type to use when publishing data to Redis, either `json`, `msgpack` or `bincode`                        | `json`              |
+| `REDIS_KISS_LENIENT`  | Whether to try to automatically decode other payload types if we fail to deserialize it using the chosen one | `1`                 |
---
+++ src/lib.rs
@@ -0,0 +1,230 @@
+use std::env;
+
+use mobc::Pool;
+use mobc_redis::{
+    redis::{AsyncCommands, ToRedisArgs},
+    RedisConnectionManager,
+};
+use redis::{
+    aio::{Connection, PubSub},
+    Msg,
+};
+
+use serde::{de::DeserializeOwned, Serialize};
+
+#[macro_use]
+extern crate lazy_static;
+
+#[derive(PartialEq)]
+pub enum PayloadType {
+    Json,
+    Msgpack,
+    Bincode,
+}
+
+#[derive(Debug)]
+pub enum Error {
+    FailedConnection,
+    SerFailed,
+    DeserFailed,
+    PublishFailed,
+}
+
+lazy_static! {
+    pub static ref REDIS_URI: String =
+        env::var("REDIS_URI").unwrap_or_else(|_| "redis://localhost".into());
+    static ref REDIS_POOL: Pool<RedisConnectionManager> = {
+        let max_open = env::var("REDIS_POOL_MAX_OPEN")
+            .unwrap_or_else(|_| "1000".into())
+            .parse()
+            .unwrap();
+
+        let client = mobc_redis::redis::Client::open(REDIS_URI.to_string()).unwrap();
+        let manager = RedisConnectionManager::new(client);
+        Pool::builder().max_open(max_open).build(manager)
+    };
+    pub static ref REDIS_PAYLOAD_TYPE: PayloadType = {
+        match env::var("REDIS_PAYLOAD_TYPE")
+            .unwrap_or_else(|_| "".into())
+            .to_lowercase()
+            .as_str()
+        {
+            "msgpack" => PayloadType::Msgpack,
+            "bincode" => PayloadType::Bincode,
+            _ => PayloadType::Json,
+        }
+    };
+    pub static ref REDIS_KISS_LENIENT: bool =
+        env::var("REDIS_KISS_LENIENT").map_or(true, |v| v == "1");
+}
+
+pub async fn p<
+    K: ToRedisArgs + std::marker::Send + std::marker::Sync,
+    T: Serialize + std::fmt::Debug,
+>(
+    channel: K,
+    data: T,
+) {
+    if let Err(error) = publish(channel, data).await {
+        eprintln!("Encountered an error in redis-kiss: {error:?}");
+    }
+}
+
+pub async fn publish<
+    K: ToRedisArgs + std::marker::Send + std::marker::Sync,
+    T: Serialize + std::fmt::Debug,
+>(
+    channel: K,
+    data: T,
+) -> Result<(), Error> {
+    let mut conn = REDIS_POOL
+        .get()
+        .await
+        .map_err(|_| Error::FailedConnection)?;
+
+    let _: () = match *REDIS_PAYLOAD_TYPE {
+        PayloadType::Json => conn
+            .publish(channel, serde_json::json!(data).to_string())
+            .await
+            .map_err(|_| Error::PublishFailed)?,
+        PayloadType::Bincode => conn
+            .publish(
+                channel,
+                bincode::serialize(&data).map_err(|_| Error::SerFailed)?,
+            )
+            .await
+            .map_err(|_| Error::PublishFailed)?,
+        PayloadType::Msgpack => conn
+            .publish(
+                channel,
+                rmp_serde::to_vec_named(&data).map_err(|_| Error::SerFailed)?,
+            )
+            .await
+            .map_err(|_| Error::PublishFailed)?,
+    };
+
+    Ok(())
+}
+
+pub async fn open_pubsub_connection() -> Result<PubSub, Error> {
+    redis::Client::open(REDIS_URI.to_string())
+        .map_err(|_| Error::FailedConnection)?
+        .get_async_connection()
+        .await
+        .map(Connection::into_pubsub)
+        .map_err(|_| Error::FailedConnection)
+}
+
+pub async fn decode_payload_using<T: DeserializeOwned>(
+    msg: &Msg,
+    target: &PayloadType,
+) -> Result<T, Error> {
+    Ok(match target {
+        PayloadType::Json => serde_json::from_str(
+            &msg.get_payload::<String>()
+                .map_err(|_| Error::DeserFailed)?,
+        )
+        .map_err(|_| Error::DeserFailed)?,
+        PayloadType::Msgpack => {
+            rmp_serde::from_slice(msg.get_payload_bytes()).map_err(|_| Error::DeserFailed)?
+        }
+        PayloadType::Bincode => {
+            bincode::deserialize(msg.get_payload_bytes()).map_err(|_| Error::DeserFailed)?
+        }
+    })
+}
+
+pub async fn decode_payload<T: DeserializeOwned>(msg: &Msg) -> Result<T, Error> {
+    let pt = &*REDIS_PAYLOAD_TYPE;
+    match decode_payload_using::<T>(msg, pt).await {
+        Err(error) => {
+            if !(*REDIS_KISS_LENIENT) {
+                return Err(error);
+            }
+
+            if pt != &PayloadType::Json {
+                if let Ok(v) = decode_payload_using(msg, &PayloadType::Json).await {
+                    return Ok(v);
+                }
+            }
+
+            if pt != &PayloadType::Msgpack {
+                if let Ok(v) = decode_payload_using(msg, &PayloadType::Msgpack).await {
+                    return Ok(v);
+                }
+            }
+
+            if pt != &PayloadType::Bincode {
+                if let Ok(v) = decode_payload_using(msg, &PayloadType::Bincode).await {
+                    return Ok(v);
+                }
+            }
+
+            Err(Error::DeserFailed)
+        }
+        v => v,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::{select, FutureExt, StreamExt};
+
+    #[async_std::test]
+    async fn publish_json() {
+        crate::p("test", "sus").await;
+    }
+
+    #[async_std::test]
+    async fn publish_bincode() {
+        std::env::set_var("REDIS_PAYLOAD_TYPE", "bincode");
+        crate::p("test", "sus").await;
+    }
+
+    #[async_std::test]
+    async fn publish_msgpack() {
+        std::env::set_var("REDIS_PAYLOAD_TYPE", "msgpack");
+        crate::p("test", "sus").await;
+    }
+
+    #[async_std::test]
+    async fn subscribe() {
+        if let Ok(mut conn) = crate::open_pubsub_connection().await {
+            conn.subscribe("test").await.unwrap();
+            conn.subscribe("neat").await.unwrap();
+            crate::p("neat", "epic").await;
+
+            async fn handle_messages(conn: &mut redis::aio::PubSub) {
+                loop {
+                    {
+                        let mut stream = conn.on_message();
+                        while let Some(item) = stream.next().await {
+                            let topic: String = item.get_channel_name().to_string();
+                            if let Ok(v) = crate::decode_payload::<String>(&item).await {
+                                dbg!(&topic, v);
+
+                                if topic == "test" {
+                                    break;
+                                }
+                            } else {
+                                eprintln!("deser failed!")
+                            }
+                        }
+                    }
+
+                    conn.subscribe("deez").await.unwrap();
+                }
+            }
+
+            async fn worker_task() {
+                crate::p("test", "sus").await;
+                crate::p("deez", "nuts").await;
+            }
+
+            select! {
+                () = handle_messages(&mut conn).fuse() => {},
+                () = worker_task().fuse() => {},
+            }
+        }
+    }
+}
